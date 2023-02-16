4d05e9ed1e93a723aa37f53797e53c10f1653fc4
inmove <lisper.inmove@gmail.com>
2023-02-16 11:34:13 +0800

    update gview_port.py

--- view/gview_port.py
+++ view/gview_port.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import inspect
 from concurrent import futures
 
 import grpc
@@ -12,38 +13,37 @@ from view.view_helper import ViewHelper
 logger = Logger()
 
 
-class Api(BaseCls):
+class ApiMeta(type):
 
-    source_ctrls = dict()
+    def __new__(cls, name, bases, attrs):
+        view_helper = ViewHelper("ctrl/gctrl")
+        view_helper.load_ctrl()
+        for func in inspect.getmembers(api_pb_grpc.APIServicer, predicate=inspect.isfunction):
+            ctrl_name = func[0].split("__")[0]
+            func_name = func[0].split("__")[-1]
+            ctrl = view_helper.ctrls.get(ctrl_name)
+            if not ctrl:
+                attrs.update({func[0]: cls.method_not_implemented})
+            else:
+                attrs.update({func[0]: cls.api_creator(func_name, ctrl)})
+        return super().__new__(cls, name, bases, attrs)
 
-    def __init__(self, *args, **kargs):
-        super().__init__(*args, **kargs)
-        self.view_helper = ViewHelper("ctrl/gctrl")
-        self.view_helper.load_ctrl()
+    @classmethod
+    def api_creator(cls, name, ctrl):
+        def func(self, req, ctx):
+            obj = ctrl(req, ctx)
+            logger.info(f"{name} 被调用: {req}")
+            if not hasattr(obj, name):
+                raise NotImplementedError("Method not implemented!")
+            return getattr(obj, name)()
+        return func
 
-    def __getattr__(self, key):
-        """获取属性."""
-        key_split = key.split("__")
-        if key_split[0] not in self.view_helper.ctrls:
-            return super().__getattr__(key)
-        source = key_split[0].replace("_", "-")
-        api_name = "_".join(key_split[1:])
-        ctrl_obj = self.source_ctrls.get(source)
-        if ctrl_obj:
-            if hasattr(ctrl_obj, api_name):
-                return getattr(ctrl_obj, api_name)
-            return self.not_implemented(api_name)
-        ctrl_cls = self.view_helper.ctrls[source]
-        if not hasattr(ctrl_cls, api_name):
-            return self.not_implemented(api_name)
-        cls_obj = ctrl_cls()
-        self.source_ctrls.update({source: cls_obj})
-        return getattr(cls_obj, api_name)
+    @classmethod
+    def method_not_implemented(cls, req, ctx):
+        raise NotImplementedError("Method not implemented!")
 
-    def not_implemented(self, api_name):
-        def m(req, ctx):
-            raise NotImplementedError(f"{api_name} Not Implemented!")
-        return m
+
+class Api(BaseCls,  api_pb_grpc.APIServicer, metaclass=ApiMeta):
 
     def run(self, host, port, max_workers=10):
         logger.info(f">>>>> {self.app_name} grpc 服务已启动: {host} {port} {max_workers} <<<<<")
