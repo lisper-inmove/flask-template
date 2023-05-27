50e446493973be179aee59d41ce37186c4e9fed1
inmove <lisper.inmove@gmail.com>
2023-05-27 12:32:18 +0800

    登陆接口返回token过期时间

--- ctrl/hctrl/user/user.py
+++ ctrl/hctrl/user/user.py
@@ -2,6 +2,7 @@ import api.user_api_pb2 as api_pb
 from ctrl.base_ctrl import BaseCtrl
 from manager.user.user import UserManager
 from submodules.utils.jwt_util import JWTUtil
+from submodules.utils.idate import IDate
 from view.errors import PopupError
 
 
@@ -17,6 +18,7 @@ class UserCtrl(BaseCtrl):
         manager.add_or_update_user(user)
         resp = api_pb.CommonUserResponse()
         resp.username = user.username
+        resp.token_expire_at = IDate.now_timestamp() + JWTUtil.TOKEN_VALID_TIME_PERIOD
         resp.token = JWTUtil().generate_token(
             payload={"username": user.username, "id": user.id})
         return resp
@@ -30,6 +32,7 @@ class UserCtrl(BaseCtrl):
         user = manager.login(req)
         resp = api_pb.CommonUserResponse()
         resp.username = user.username
+        resp.token_expire_at = IDate.now_timestamp() + JWTUtil.TOKEN_VALID_TIME_PERIOD
         resp.token = JWTUtil().generate_token(
             payload={"username": user.username, "id": user.id})
         return resp
